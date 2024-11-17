19b76e82bfd76c841c5fc1eee7c9686145d1eb0e
Andrey de S. Almeida <andrey0404.as@gmail.com>
2024-11-16 22:00:11 -0300

    Rota de upload do arquivo csv

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './src/routes';
 
 const app = express()
+app.use(cors())
 const PORT = 3000
 app.use(express.json())
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import userRoutes from './user.route';
+import uploadRoute from './file.route';
 
 const router = Router();
 
 router.use('/users',userRoutes);
+router.use('/upload', uploadRoute);
 export default router;
